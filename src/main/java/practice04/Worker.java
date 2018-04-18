e43c2c00cc77a589264f8a3bea35d65930a06d8f
weiyang1024 <weiyang_work@163.com>
2018-04-18 15:33:52 +0800

    Finish practice04

--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,37 @@
 package practice04;
 
-public class Worker {
+public class Worker extends Person {
+    private String name;
+    private int age;
+
+    public Worker(String sName, int iAge) {
+        super(sName, iAge);
+        this.name = sName;
+        this.age = iAge;
+
+    }
+
+    public String introduce() {
+        return super.introduce() + " I am a Worker. I have a job.";
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
