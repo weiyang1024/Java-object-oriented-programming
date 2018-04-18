b863b4a72892b4d28e2d112d47f773f9a854e665
weiyang1024 <weiyang_work@163.com>
2018-04-18 15:03:50 +0800

    Finish practice02

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,43 @@
 package practice02;
 
-public class Student {
+public class Student extends Person{
+    private int klass;
+    private String name;
+    private int age;
+    public Student(String sName, int iAge,int iKlass) {
+        super(sName, iAge);
+        this.name = sName;
+        this.age = iAge;
+        this.klass = iKlass;
+
+    }
+    public String introduce() {
+        return "I am a Student. I am at Class "+Integer.toString(klass) + ".";
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+    public int getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
