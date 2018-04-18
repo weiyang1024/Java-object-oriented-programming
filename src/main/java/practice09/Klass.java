8b6e2f026ed9c69e42cb65deeb1c1789a6ce379f
weiyang1024 <weiyang_work@163.com>
2018-04-18 20:08:07 +0800

    Finish practice09

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,45 @@
 package practice09;
 
+import java.util.ArrayList;
+
 public class Klass {
+    private int className;
+    private Student leader;
+    private ArrayList<Integer> studentId;
+
+    public Klass(int iName) {
+        this.className = iName;
+        studentId = new ArrayList<>();
+    }
+
+    public int getClassName() {
+        return className;
+    }
+
+    public void setClassName(int className) {
+        this.className = className;
+    }
+
+    public int getNumber() {
+        return className;
+    }
+
+    public String getDisplayName() {
+        return "Class " + Integer.toString(className);
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void assignLeader(Student leader) {
+        if (studentId != null && studentId.contains(leader.getId()))
+            this.leader = leader;
+        else
+            System.out.print("It is not one of us.\n");
+    }
+
+    public void appendMember(Student student) {
+        studentId.add(student.getId());
+    }
 }
--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,66 @@
 package practice09;
 
-public class Teacher {
+public class Teacher extends Person {
+    private int id;
+    private Klass klass;
+    private String name;
+    private int age;
+
+    public Teacher(int iId, String sName, int iAge, Klass oKlass) {
+        super(iId, sName, iAge);
+        this.id = iId;
+        this.name = sName;
+        this.age = iAge;
+        this.klass = oKlass;
+    }
+
+    public Teacher(int iId, String sName, int iAge) {
+        super(iId, sName, iAge);
+        this.id = iId;
+        this.name = sName;
+        this.age = iAge;
+    }
+
+    public String introduce() {
+        String result = super.introduce() + " I am a Teacher.";
+        if (klass != null)
+            return result + " I teach Class " + Integer.toString(klass.getClassName()) + ".";
+        else
+            return result + " I teach No Class.";
+    }
+
+    public String introduceWith(Student student) {
+        if (klass.getClassName() == student.getKlass().getClassName())
+            return super.introduce() + " I am a Teacher. I teach " + student.getName() + ".";
+        else
+            return super.introduce() + " I am a Teacher. I don't teach " + student.getName() + ".";
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
