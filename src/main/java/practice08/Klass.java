113f822299a5fc5b4e9e456017954d3b748fabac
weiyang1024 <weiyang_work@163.com>
2018-04-18 17:54:53 +0800

    Finish practice08

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,33 @@
 package practice08;
 
 public class Klass {
+    private int className;
+    private Student leader;
+
+    public Klass(int iName){
+        this.className = iName;
+    }
+    public int getClassName() {
+        return className;
+    }
+
+    public void setClassName(int className) {
+        this.className = className;
+    }
+
+    public int getNumber() {
+        return className;
+    }
+
+    public String getDisplayName() {
+        return "Class " + Integer.toString(className);
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void assignLeader(Student leader) {
+        this.leader = leader;
+    }
 }
--- src/main/java/practice08/Student.java
+++ src/main/java/practice08/Student.java
@@ -1,4 +1,62 @@
 package practice08;
 
-public class Student {
+public class Student extends Person {
+    private int id;
+    private Klass klass;
+    private String name;
+    private int age;
+
+    public Student(int iId, String sName, int iAge, Klass oKlass) {
+        super(iId, sName, iAge);
+        this.id = iId;
+        this.name = sName;
+        this.age = iAge;
+        this.klass = oKlass;
+
+    }
+
+    public String introduce() {
+        if (klass.getLeader() != null && klass.getLeader().getId() == id)
+            return super.introduce() + " I am a Student. I am Leader of Class " + Integer.toString(klass.getClassName()) + ".";
+        else
+            return super.introduce() + " I am a Student. I am at Class " + Integer.toString(klass.getClassName()) + ".";
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(int id) {
+        this.id = id;
+    }
 }
